ea707be9128a823e5078b1a8b13d34422cabc14f
Haley T <hntrimpert@yahoo.com>
2023-04-04 18:02:51 -0400

    models index.js start

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Product = require('./Product');
+const ProductTag = require('./ProductTag');
+const Review = require('./Review');
+const Tag = require('./Tag');
+const User = require('./User');
+const UserProduct = require('./UserProduct');
+
