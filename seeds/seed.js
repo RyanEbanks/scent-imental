42f944e31b0ffe83f5b09d9eacedf9dab58f12a6
abmetheny <abmetheny@gmail.com>
2023-04-04 21:26:52 -0400

    Updated json seeds and populated seed.js file

---
+++ seeds/seed.js
@@ -0,0 +1,32 @@
+const sequelize = require('../config/connection');
+const { Product, ProductTag, Review, Tag, User, UserProduct } = require('../models');
+
+const productData = require('./productData.json');
+const productTagData = require('./productTagData.json');
+const reviewData = require('./reviewData.json');
+const tagData = require('./tagData.json');
+const userData = require('./userData.json');
+const userProductData = require('./userProductData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const products = await Product.bulkCreate(productData);
+
+  const productTags = await ProductTag.bulkCreate(productTagData);
+
+  const reviews = await Review.bulkCreate(reviewData);
+
+  const tagData = await Tag.bulkCreate(tagData);
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const userProductData = await Tag.bulkCreate(userProductData);
+
+  process.exit(0);
+};
+
+seedDatabase();
