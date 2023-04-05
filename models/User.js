984e61c262352e86f4ae18c919e30a436b4dc474
Haley T <hntrimpert@yahoo.com>
2023-04-04 20:03:06 -0400

    populated data structure in user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require("sequelize");
+const Sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8],
+    },
+  },
+});
+
+module.exports = User;
